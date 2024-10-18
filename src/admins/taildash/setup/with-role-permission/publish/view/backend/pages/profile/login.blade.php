eba2c3dd29102bccd5fa63a962394237a5ea5fab
Md. Nobir Hasan <nobir.wd@gmail.com>
2024-10-18 18:25:27 +0600

    Currecting v2.5 (#19)
    * Update TheBackendWizardCommand.php
    
    * Fix styling
    
    * setup done
    
    * Fix styling
    
    * Update filelocation.php
    
    * done. no error
    
    * Fix styling
    
    * Update login.blade.php
    
    ---------
    
    Co-authored-by: md-nobir-hasan <md-nobir-hasan@users.noreply.github.com>

--- src/admins/taildash/setup/with-role-permission/publish/view/backend/pages/profile/login.blade.php
+++ src/admins/taildash/setup/with-role-permission/publish/view/backend/pages/profile/login.blade.php
@@ -10,7 +10,7 @@
 <body>
     <section class="bg-gray-50 dark:bg-gray-900">
         <div class="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
-            <a href="#" class="mb-6 flex items-center text-2xl font-semibold text-gray-900 dark:text-white">
+            <a href="{{route('home')}}" class="mb-6 flex items-center text-2xl font-semibold text-gray-900 dark:text-white">
                 {{-- setting @if ($setting->logo)
                     <img class="mr-2 h-8 w-8" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
                     alt="logo">
